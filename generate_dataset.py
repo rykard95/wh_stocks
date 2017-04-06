90952325a8c5d9e5f0c3d808c45ef51e0308ae56
Richard Doan <rdoan@berkeley.edu>
2017-04-06 10:51:42 -0700

    added white host post pulling

---
+++ generate_dataset.py
@@ -0,0 +1,102 @@
+import csv
+from datetime import date, timedelta
+from pickle import dump, load
+from calendar import month_name
+import numpy as np
+import pandas as pd
+import requests
+import bs4
+from yahoo_finance import Share
+
+
+INDEX_NAMES = {'Nasdaq': '^IXIC', 'Dow Jones': '^DJI', 'S&P 500': '^GSPC'}
+
+def generate_wh_data():
+	# new list to append urls
+	post_urls = []
+	url = "https://www.whitehouse.gov"
+
+	# 50 pages of posts
+	for i in np.arange(50):
+	    #grab url and append page number
+	    page_url = url + "/blog?page=" + str(i)
+	    r = requests.get(page_url)
+	    soup = bs4.BeautifulSoup(r.content.decode('utf-8'), "lxml")
+	    # h3 field-content is the tag to get post urls
+	    page_posts = soup.find_all("h3", "field-content")
+	    # dates for the posts
+	    post_dates = soup.find_all("span", "field-content")
+	    # append page's post urls to our list
+	    post_urls.append([(post.text, post.find('a')['href'], date.text) for post, date in zip(page_posts, post_dates)])
+
+	# flatten list
+	all_posts = [p for post in post_urls for p in post]
+
+	posts = []
+
+	# loop through every post and get text in post
+	for post_data in all_posts:
+	    req = requests.get(url + post_data[1])
+	    req_soup = bs4.BeautifulSoup(req.text, "lxml")
+	    post_body = req_soup.find("div","pane-entity-field").text\
+	    .encode('ascii', 'ignore').decode('UTF-8').replace("\n", "").replace("\t", "")
+	    posts.append(post_body)
+	        
+	df_out = pd.DataFrame({'a': [post[2].strip() for post in all_posts], 'b': [post[0] for post in all_posts], 'c': [post.strip() for post in posts]})
+	df_out.columns = ['Date', 'Title', 'Body']
+
+	df_out.to_csv('data/WH_posts.csv', index=False)
+	return df_out
+
+def get_stock_values(stock_abbrv):
+    """
+    Given the stock abbrevation, this function
+    will pull from Yahoo Finance the history of that 
+    stock from 2016-12-25 to present day
+    """
+    share = Share(stock_abbrv)
+    share_history = share.get_historical('2016-12-25', date.isoformat(date.today()))
+    df = pd.DataFrame([[s['Date'], float(s['Close'])] for s in share_history], columns=['Date', 'Value'])
+    return df
+
+def create_dataset(regenerate=False):
+    """
+    Pulls closing values from Yahoo finance and
+    matches those values to white house posts using
+    the date field. Returns a dataframe for easy
+    manipulation.
+    """
+
+    if regenerate:
+    	wh_df = generate_wh_data()
+    else:
+    	wh_df = pd.read_csv('data/WH_posts.csv')
+    wh_df['Date'] = pd.to_datetime(wh_df['Date'])
+        
+    stock_dfs = {name:get_stock_values(INDEX_NAMES[name]) for name in INDEX_NAMES}
+    processed_stock_dfs = []
+    for name in stock_dfs:
+        stock_df = stock_dfs[name]
+        dates = pd.to_datetime(stock_df['Date'])
+        values = stock_df['Value'].rename(name + ' Value')
+        stock_df = pd.concat([dates, values], axis=1, join_axes=[dates.index])
+        stock_df.sort_values(by='Date')
+        
+        stock_df[name + ' Delta'] = stock_df[name + ' Value'].diff(periods=1)
+        processed_stock_dfs.append(stock_df)     
+
+        
+    wh_df['Date'] = pd.to_datetime(wh_df['Date'])
+    stock_df = processed_stock_dfs[0]
+    for i in range(1, len(stock_dfs)):
+        stock_df = pd.merge(stock_df, processed_stock_dfs[i], how='inner', on=['Date'])
+    
+    dataset = pd.merge(wh_df, stock_df, how='inner', on=['Date'])
+    dataset.to_csv('data/dataset.csv')
+    
+    return dataset.sort_values(by='Date').reset_index(drop=True)
+    
+    
+if __name__ == "__main__":
+	create_dataset()
+    

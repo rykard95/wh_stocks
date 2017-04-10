059e55ace45d9cabcbe2f2bfe4d6d6563808e96d
Richard Doan <rdoan@berkeley.edu>
2017-04-10 13:25:26 -0700

    added richard scripts

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-import csv
-from datetime import date, timedelta
-from pickle import dump, load
-from calendar import month_name
+from datetime import date
 import numpy as np
 import pandas as pd
 import requests
@@ -14,47 +11,47 @@ from yahoo_finance import Share
 INDEX_NAMES = {'Nasdaq': '^IXIC', 'Dow Jones': '^DJI', 'S&P 500': '^GSPC'}
 
 def generate_wh_data(n):
-	"""
-	Pulls White House Posts and writes them to 
-	data/WH_posts.csv
-	"""
-	# new list to append urls
-	post_urls = []
-	url = "https://www.whitehouse.gov"
+    """
+    Pulls White House Posts and writes them to 
+    data/WH_posts.csv
+    """
+    # new list to append urls
+    post_urls = []
+    url = "https://www.whitehouse.gov"
 
-	print("Pulling White House posts")
-	for i in np.arange(n):
-	    #grab url and append page number
-	    page_url = url + "/blog?page=" + str(i)
-	    r = requests.get(page_url)
-	    soup = bs4.BeautifulSoup(r.content.decode('utf-8'), "html.parser")
-	    # h3 field-content is the tag to get post urls
-	    page_posts = soup.find_all("h3", "field-content")
-	    # dates for the posts
-	    post_dates = soup.find_all("span", "field-content")
-	    # append page's post urls to our list
-	    post_urls.append([(post.text, post.find('a')['href'], date.text) for post, date in zip(page_posts, post_dates)])
+    print("Pulling White House posts")
+    for i in np.arange(n):
+        #grab url and append page number
+        page_url = url + "/blog?page=" + str(i)
+        r = requests.get(page_url)
+        soup = bs4.BeautifulSoup(r.content.decode('utf-8'), "html.parser")
+        # h3 field-content is the tag to get post urls
+        page_posts = soup.find_all("h3", "field-content")
+        # dates for the posts
+        post_dates = soup.find_all("span", "field-content")
+        # append page's post urls to our list
+        post_urls.append([(post.text, post.find('a')['href'], date.text) for post, date in zip(page_posts, post_dates)])
 
-	# flatten list
-	all_posts = [p for post in post_urls for p in post]
+    # flatten list
+    all_posts = [p for post in post_urls for p in post]
 
-	posts = []
+    posts = []
 
-	# loop through every post and get text in post
-	print("Parsing each post")
-	for post_data in all_posts:
-	    req = requests.get(url + post_data[1])
-	    req_soup = bs4.BeautifulSoup(req.text, "html.parser")
-	    post_body = req_soup.find("div","pane-entity-field").text\
-	    .encode('ascii', 'ignore').decode('UTF-8').replace("\n", "").replace("\t", "")
-	    posts.append(post_body)
-	        
-	df_out = pd.DataFrame({'a': [post[2].strip() for post in all_posts], 'b': [post[0] for post in all_posts], 'c': [post.strip() for post in posts]})
-	df_out.columns = ['Date', 'Title', 'Body']
+    # loop through every post and get text in post
+    print("Parsing each post")
+    for post_data in all_posts:
+        req = requests.get(url + post_data[1])
+        req_soup = bs4.BeautifulSoup(req.text, "html.parser")
+        post_body = req_soup.find("div","pane-entity-field").text\
+        .encode('ascii', 'ignore').decode('UTF-8').replace("\n", "").replace("\t", "")
+        posts.append(post_body)
 
-	print("Writing data/WH_posts.csv")
-	df_out.to_csv('data/WH_posts.csv', index=False)
-	return df_out
+    df_out = pd.DataFrame({'a': [post[2].strip() for post in all_posts], 'b': [post[0] for post in all_posts], 'c': [post.strip() for post in posts]})
+    df_out.columns = ['Date', 'Title', 'Body']
+
+    print("Writing data/WH_posts.csv")
+    df_out.to_csv('data/WH_posts.csv', index=False)
+    return df_out
 
 
 def get_stock_values(stock_abbrv):
@@ -78,11 +75,11 @@ def create_dataset(regenerate=False, n=50):
     """
 
     if regenerate:
-    	wh_df = generate_wh_data(n)
+        wh_df = generate_wh_data(n)
     else:
-    	wh_df = pd.read_csv('data/WH_posts.csv')
+        wh_df = pd.read_csv('data/WH_posts.csv')
     wh_df['Date'] = pd.to_datetime(wh_df['Date'])
-        
+
     stock_dfs = {name:get_stock_values(INDEX_NAMES[name]) for name in INDEX_NAMES}
     processed_stock_dfs = []
     for name in stock_dfs:
@@ -91,30 +88,29 @@ def create_dataset(regenerate=False, n=50):
         values = stock_df['Value'].rename(name + ' Value')
         stock_df = pd.concat([dates, values], axis=1, join_axes=[dates.index])
         stock_df.sort_values(by='Date')
-        
-        stock_df[name + ' Delta'] = stock_df[name + ' Value'].diff(periods=1)
-        processed_stock_dfs.append(stock_df)     
 
-        
+        stock_df[name + ' Delta'] = -stock_df[name + ' Value'].diff(periods=1)
+        processed_stock_dfs.append(stock_df)
+
+
     wh_df['Date'] = pd.to_datetime(wh_df['Date'])
     stock_df = processed_stock_dfs[0]
     for i in range(1, len(stock_dfs)):
         stock_df = pd.merge(stock_df, processed_stock_dfs[i], how='inner', on=['Date'])
-    
+
     dataset = pd.merge(wh_df, stock_df, how='inner', on=['Date'])
     dataset.to_csv('data/dataset.csv', index=False)
-    
+
     return dataset.sort_values(by='Date').reset_index(drop=True)
     
     
 if __name__ == "__main__":
-	from argparse import ArgumentParser
-	parser = ArgumentParser(description='White House Post and Stock Market Collector')
-	parser.add_argument('-r', '--regen', action='store_true',
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='White House Post and Stock Market Collector')
+    parser.add_argument('-r', '--regen', action='store_true',
                     help='Regenerates WH_post.csv')
-	parser.add_argument('-n', '--num_pages', type=int,
-					help='Number of pages to pull')
-	args = parser.parse_args()
+    parser.add_argument('-n', '--num_pages', type=int,
+                    help='Number of pages to pull')
+    args = parser.parse_args()
 
-	create_dataset(regenerate=args.regen, n=args.num_pages)
-    
+    create_dataset(regenerate=args.regen, n=args.num_pages)
